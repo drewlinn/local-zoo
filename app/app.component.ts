fd7d9358b2d6696f12d9c05ad35a75aa6de53252
Andrew <expandrew@gmail.com>
2017-07-05 14:19:10 -0700

    incorporate date and focus functionality in app.component.js

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,12 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <div class="container">
-    <h1>My First Angular 2 App</h1>
+   <div class="container">
+      <h1>Angular 2 App for {{month}}/{{day}}/{{year}}</h1>
+      <h3>{{currentFocus}}</h3>
   </div>
   `
 })
 
 export class AppComponent {
-
+  currentFocus: string = 'Angular Homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
 }
